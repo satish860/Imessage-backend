b2d716400064d72dd0c7b757edc22eb9f4da2f6c
Satish Venkatakrishnan <satish860@gmail.com>
2023-03-02 11:51:24 +0530

    Added the search to Graphql API

--- src/IMessager.Api/HttpRequestInterceptor.cs
+++ src/IMessager.Api/HttpRequestInterceptor.cs
@@ -1,5 +1,6 @@
 ﻿using HotChocolate.AspNetCore;
 using HotChocolate.Execution;
+using IMessager.Api.Session;
 using IMessager.Api.Types;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using MongoDB.Bson;
@@ -12,11 +13,13 @@ namespace IMessager.Api
     {
         private readonly IMongoDatabase database;
         private readonly IMongoCollection<Session.Session> sessionCollection;
+        private readonly IMongoCollection<Session.User> userCollection;
 
         public HttpRequestInterceptor(IMongoDatabase database)
         {
             this.database = database;
             this.sessionCollection = database.GetCollection<Session.Session>("Session");
+            this.userCollection = database.GetCollection<Session.User>("User");
         }
 
         public override ValueTask OnCreateAsync(HttpContext context,
@@ -27,7 +30,7 @@ namespace IMessager.Api
             var req = context.Request;
             var sessionToken = req.Cookies
                         .FirstOrDefault(p => p.Key == "next-auth.session-token")!.Value;
-           
+
             if (sessionToken != null)
             {
                 var entity = sessionCollection
@@ -37,6 +40,14 @@ namespace IMessager.Api
                 {
                     ClaimsIdentity claimsIdentity = new ClaimsIdentity(JwtBearerDefaults.AuthenticationScheme);
                     claimsIdentity.AddClaim(new Claim("userId", entity.UserId));
+                    var user = userCollection
+                                .Find(doc => doc.Id == ObjectId.Parse(entity.UserId))
+                                .FirstOrDefault();
+                    if (user != null && user.UserName != null)
+                    {
+                        claimsIdentity.AddClaim(new Claim("userName", user!.UserName));
+                    }
+
                     context.User.AddIdentity(claimsIdentity);
                 }
 
--- src/IMessager.Api/Types/User.cs
+++ src/IMessager.Api/Types/User.cs
@@ -7,16 +7,47 @@ namespace IMessager.Api.Types
 {
     public class User
     {
-        public string Id { get; set; }
+        public string? Id { get; set; }
 
-        public string UserName { get; set; }
+        public string? UserName { get; set; }
     }
 
     public class Query
     {
-        public IEnumerable<User> SearchUsers(string username)
+        private readonly IMongoDatabase database;
+        private readonly IMongoCollection<Session.User> sessionCollection;
+
+        public Query(IMongoDatabase database)
         {
-            return Enumerable.Empty<User>();
+            this.database = database;
+            this.sessionCollection = database.GetCollection<Session.User>("User");
+        }
+
+        public async Task<IEnumerable<User>> SearchUsers(string username, ClaimsPrincipal claimsPrincipal)
+        {
+            var identity = claimsPrincipal
+                        .Identities
+                        .FirstOrDefault(identity => identity.AuthenticationType == JwtBearerDefaults.AuthenticationScheme);
+            var userId = identity!.Claims.FirstOrDefault(p => p.Type == "userId")!.Value;
+            var searchBuilder = Builders<Session.User>
+                .Filter
+                .Regex(u => u.UserName, new BsonRegularExpression(username, "i"));
+            var notInSameuser = Builders<Session.User>
+                .Filter
+                .Ne(p=>p.Id,ObjectId.Parse(userId));
+            var filter = Builders<Session.User>
+                .Filter
+                .And(searchBuilder, notInSameuser);
+            var results = await this.sessionCollection.Find(filter).ToListAsync();
+            var users = results.Select(p =>
+             {
+                 return new User
+                 {
+                     Id = p.Id.ToString(),
+                     UserName = p.UserName!
+                 };
+             });
+            return users;
         }
     }
 
@@ -37,12 +68,12 @@ namespace IMessager.Api.Types
                         .Identities
                         .FirstOrDefault(identity => identity.AuthenticationType == JwtBearerDefaults.AuthenticationScheme);
             var user = identity!.Claims.FirstOrDefault(p => p.Type == "userId")!.Value;
-            if (user != null) 
+            if (user != null)
             {
                 var entity = sessionCollection
                   .Find(doc => doc.Id == ObjectId.Parse(user))
                   .FirstOrDefault();
-                if (entity != null && entity.UserName==null)
+                if (entity != null && entity.UserName == null)
                 {
                     entity.UserName = username;
                     var filter = Builders<Session.User>
